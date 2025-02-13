7bf6e911716ecfab3cafd4e0028d3f7fb19aea17
Daniel O'Hanlon <dpohanlon@gmail.com>
2025-02-13 16:41:05 +0000

    Add a function to run nb fits for all genes.

--- fast_negative_binomial/bindings.cpp
+++ fast_negative_binomial/bindings.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "fast_nb.cpp"
+#include "optimise.hpp"
 
 namespace py = pybind11;
 
@@ -169,4 +170,43 @@ PYBIND11_MODULE(fast_negative_binomial, m) {
                 np.array: Gradients of m, r. Nsamples x 2
         )pbdoc");
 
+    m.def(
+        "optimise",
+        [](Eigen::VectorXi & data, double m, double r, double learning_rate, int max_iterations) -> std::pair<double, double> {
+            return optimise(data, m, r, learning_rate, max_iterations);
+        },
+        py::arg("data"), py::arg("m") = 10.0, py::arg("r") = 10.0, py::arg("learning_rate") = 0.01, py::arg("max_iterations") = 1000,
+        R"pbdoc(
+            Fit a negative binomial pmf on the data
+
+            Parameters:
+                k (np.array[int]): Observations
+                m (float) : initial m value
+                r (float) : initial r value
+                learning_rate (float): Learning rate for gradient descent
+                max_iterations (float): Max number of gradient descent iterations
+
+            Returns:
+                np.array: optimised m, r
+        )pbdoc");
+
+    m.def(
+        "optimise_all_genes",
+        [](Eigen::MatrixXi & data, double m, double r, double learning_rate, int max_iterations) -> std::vector<std::pair<double, double>> {
+            return optimise_all_genes(data, m, r, learning_rate, max_iterations);
+        },
+        py::arg("data"), py::arg("m") = 10.0, py::arg("r") = 10.0, py::arg("learning_rate") = 0.01, py::arg("max_iterations") = 1000,
+        R"pbdoc(
+            Fit a negative binomial pmf on the data
+
+            Parameters:
+                k (np.array[int]): Counts matrix, nGenes x nCells ????
+                m (float) : initial m value
+                r (float) : initial r value
+                learning_rate (float): Learning rate for gradient descent
+                max_iterations (float): Max number of gradient descent iterations
+
+            Returns:
+                np.array: optimised m, r for each gene
+        )pbdoc");
 }
--- fast_negative_binomial/optimise.hpp
+++ fast_negative_binomial/optimise.hpp
@@ -2,13 +2,12 @@
 #include <Eigen/Dense>
 #include <algorithm>
 
+#include "omp.h"
+
 #include "eigen_nb.hpp"
 #include "eigen_nb_jac.hpp"
 
-std::pair<double, double> optimise(Eigen::VectorX & k, double learning_rate = 1E-2, int max_iterations = 1000) {
-
-    double m = 10.0;
-    double r = 10.0;
+std::pair<double, double> optimise(Eigen::VectorXi & k, double m = 10., double r = 10., double learning_rate = 1E-2, int max_iterations = 1000) {
 
     const double tolerance = 1e-6;
 
@@ -21,7 +20,6 @@ std::pair<double, double> optimise(Eigen::VectorX & k, double learning_rate = 1E
         Eigen::Vector2d grad = -grad_matrix.colwise().sum();
 
         if (grad.norm() < tolerance) {
-            std::cout << "Converged at iteration " << iter << std::endl;
             break;
         }
 
@@ -31,17 +29,26 @@ std::pair<double, double> optimise(Eigen::VectorX & k, double learning_rate = 1E
         m = std::max(m, 1e-8);
         r = std::max(r, 1e-8);
 
-        if (iter % 100 == 0) {
-            std::cout << "Iteration " << iter
-                      << ": Objective = " << objective
-                      << ", m = " << m
-                      << ", r = " << r << std::endl;
-        }
     }
 
-    std::cout << "Final optimized parameters:\n";
-    std::cout << "m = " << m << "\n";
-    std::cout << "r = " << r << std::endl;
-
     return std::make_pair(m, r);
+}
+
+std::vector<std::pair<double, double>> optimise_all_genes(Eigen::MatrixXi & k,
+                                                            double m = 10.,
+                                                            double r = 10.,
+                                                            double learning_rate = 1E-2,
+                                                            int max_iterations = 1000)
+{
+    // Assume that each row corresponds to one gene.
+    const int num_genes = k.rows();
+    std::vector<std::pair<double, double>> optimized_params(num_genes);
+
+    # pragma omp parallel for schedule(static)
+    for (int i = 0; i < num_genes; ++i) {
+        Eigen::VectorXi gene_data = k.row(i).transpose();
+        optimized_params[i] = optimise(gene_data, m, r, learning_rate, max_iterations);
+    }
+
+    return optimized_params;
 }
